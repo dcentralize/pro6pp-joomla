b2f6b13c6def0135a575eb4c7677cff05ecb7750
Travis <travis@travis-ci.org>
2014-11-02 16:51:01 +0000

    Travis build 4 pushed to joomla2 branch.

--- component/site/views/pro6pp/tmpl/default.php
+++
@@ -1,7 +0,0 @@
-<?php
-// No direct access to this file should be called by Joomla
-defined('_JEXEC') or die('Restricted Access');
-
-// This is the template file of the component, presents the data to the client.
-// It returns the raw reply of the service
-print($this->callback . '(' . $this->response . ')');
